036b4f09c731e40038af3da4e8bd0fc635f0b722
joscor34 <zbrm34@gmail.com>
2020-12-16 16:15:21 -0600

    Se agrego el footer de la pagina

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,44 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
     <v-main>
-      <HelloWorld/>
+      <Main/>
     </v-main>
+    <v-footer padless color="black">
+      <v-col
+        class="text-center"
+        cols="12"
+        v-on:click="goTwitter"
+      >
+       <p class="text-h6" id="footer">Made by @HoraIntelectual ❤️</p>
+      </v-col>
+    </v-footer>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import Main from './components/Main'
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld
+    Main
   },
 
   data: () => ({
     //
-  })
+  }),
+
+  methods: {
+    goTwitter: () => {
+      window.open('https://twitter.com/HoraIntelectual', '_blank')
+    }
+  }
 }
 </script>
+
+<style>
+#footer{
+  color: white;
+}
+</style>
