cf904193643d1c268929086f80605711e26dfdae
joscor34 <zbrm34@gmail.com>
2020-12-16 16:15:58 -0600

    Se agrego la pagina principal

---
+++ src/components/Main.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-container fluid fill-height>
+    <v-row>
+      <v-col cols="12" xl="12" class="text-center">
+        <h1 id="marx" class="text-h2">MARX IPSUM</h1>
+      </v-col>
+      <v-col cols="12" class="text-center">
+         <v-avatar size="200">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/f/fc/Karl_Marx.jpg"
+            alt="John"
+          >
+        </v-avatar>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Abril+Fatface&display=swap');
+#marx{
+  font-family: 'Abril Fatface', cursive;
+}
+</style>
